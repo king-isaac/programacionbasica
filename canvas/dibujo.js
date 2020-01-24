b304e024ddf758e77257f716a67554c654386220
Isaac Requena <isaacrequena@gmail.com>
2020-01-24 19:13:36 +0100

    aqui se borraron algunas lineas de comando, se trabaja con coordenadas, se uso un ciclo repetitivo con nombre de FOR, tambien se coloco dibujarlinea como lienas de borde rojo, las coordenadas son automaticas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,24 +1,24 @@
 var d = document.getElementById("dibujito"); //seleccionar el canvas
 var lienzo = d.getContext("2d"); //definir donde vamos a dibujar con su dimension
-console.log(lienzo);
-/*
-lienzo.beginPath(); //como iniciar un camino
-lienzo.strokeStyle = "red"; // color de la linea - -- esto es un atributo a diferencia de los demas
-lienzo.moveTo(100, 100); //direccion de la linea en coordenadas, desde inicio y fin
-lienzo.lineTo(200, 200); //dibujar la linea o mejor dicho "trazar"
-lienzo.stroke(); //termianador de declarar la linea
-lienzo.closePath(); //terminamos de dibujar
-/////////////////////////////////////////////////////////////////
-lienzo.beginPath(); 
-lienzo.strokeStyle = "blue"; 
-lienzo.moveTo(50, 10);
-lienzo.lineTo(250, 2); 
-lienzo.stroke(); 
-lienzo.closePath(); 
-*/
- 
-dibujarLinea("red", 10, 30, 300, 10);
-dibujarLinea("blue", 30, 10, 10, 300);
+var lineas = 30;
+var l = 0;
+var y , x;
+var borde = "red";
+
+for(l = 0; l < lineas ; l++)
+{
+	y = 10 * l;
+	x = 10 * (l + 1);
+	dibujarLinea("#AAF", 0, y, x, 300);
+	dibujarLinea("black", 300, y, x, 0);
+	
+}
+
+dibujarLinea(borde, 1, 1, 1, 299); //no le coloque 0, causa un efecto llamado "anti aliasing" se esta dibujando la mitad y no el pixel completo
+dibujarLinea(borde, 299, 1, 1, 1);
+dibujarLinea(borde, 1, 299, 299, 299);
+dibujarLinea(borde, 299, 299, 299, 1);
+
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
