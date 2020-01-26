9c36809763e7bb5cb4a3f345e0230705605149b6
Isaac Requena <isaacrequena@gmail.com>
2020-01-26 09:58:14 +0100

    segundo commit con mi practica de canvas y el pentagono, pero esta es con funciones

--- pcanvas/function.js
+++ pcanvas/function.js
@@ -1,37 +1,26 @@
 var selector_html = document.getElementById("figura1");
 var cuadro = selector_html.getContext("2d");
 
-cuadro.beginPath();
-cuadro.strokeStyle = "red";
-cuadro.moveTo(100,200);
-cuadro.lineTo(200,200);
-cuadro.stroke();
-cuadro.closePath();
+//Este codigo dibuja el romboide
+mipentagono("red",100,200,200,200);
+mipentagono("red",100,200,80,100);
+mipentagono("red",150,50,80,100);
+mipentagono("red",220,100,150,50);
+mipentagono("red",200,200,220,100);
 
-cuadro.beginPath();
-cuadro.strokeStyle = "red";
-cuadro.moveTo(80,100);
-cuadro.lineTo(100,200);
-cuadro.stroke();
-cuadro.closePath();
+//es codigo dibuja el marco o margen
+mipentagono("black",1,299,1,1);
+mipentagono("black",1,1,299,1);
+mipentagono("black",299,299,299,1);
+mipentagono("black",1,299,299,299);
 
-cuadro.beginPath();
-cuadro.strokeStyle = "red";
-cuadro.moveTo(150,50);
-cuadro.lineTo(80,100);
-cuadro.stroke();
-cuadro.closePath();
 
-cuadro.beginPath();
-cuadro.strokeStyle = "red";
-cuadro.moveTo(220,100);
-cuadro.lineTo(150,50);
-cuadro.stroke();
-cuadro.closePath();
-
-cuadro.beginPath();
-cuadro.strokeStyle = "red";
-cuadro.moveTo(200,200);
-cuadro.lineTo(220,100);
-cuadro.stroke();
-cuadro.closePath();
+function mipentagono ( color, xinicial, yinicial, xfinal, yfinal)
+{
+	cuadro.beginPath();
+	cuadro.strokeStyle = color;
+	cuadro.moveTo(xinicial,yinicial);
+	cuadro.lineTo(xfinal,yfinal);
+	cuadro.stroke();
+	cuadro.closePath();	
+}
