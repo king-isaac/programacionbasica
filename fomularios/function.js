acc8e00da52098312e053a981aefb6fbe589a4e2
Isaac Requena <isaacrequena@gmail.com>
2020-01-26 12:10:11 +0100

    otro commit donde ya esta listo el ejercicio, aqui se uno un escuchador de eventos que es element.addEventListener(evento)

--- fomularios/function.js
+++ fomularios/function.js
@@ -1,20 +1,16 @@
+// referente al form - formulario
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+
+boton.addEventListener("click", dibujoPorClick);
+
+// refente al canvas
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var lineas =30;
-var l = 0
-var yi, xf;
+var ancho = d.width;
 var rojo = "red";
 var negro = "black";
 
-for (l = 0; l < lineas; l++)
-{
-	yi = 10 * l;
-	xf = 10 * (l + 1);
-	dibujarLinea(rojo, 0, yi, xf, 300);
-}
-
-dibujarLinea(negro, 1, 1, 1, 299);
-dibujarLinea(negro, 1, 299, 299, 299);
 
 function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -30,4 +26,23 @@ function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
 	lienzo.lineTo(xfinal,yfinal);
 	lienzo.stroke();
 	lienzo.closePath();
+}
+
+
+function dibujoPorClick()
+{
+	var lineas = parseInt(texto.value);
+	var l = 0
+	var yi, xf;
+	var espacio = ancho / lineas;
+
+	for (l = 0; l < lineas; l++)
+	{
+		yi = espacio * l;
+		xf = espacio * (l + 1);
+		dibujarLinea(rojo, 0, yi, xf, 300);
+	}
+	dibujarLinea(negro, 1, 1, 1, 299);
+	dibujarLinea(negro, 1, 299, 299, 299);
+
 }
