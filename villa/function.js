a847ca5f114188175d88edfc9b77a65a167fbfb7
King-isaac <isaacrequena@gmail.com>
2020-01-27 14:15:33 -0400

    segundo commit en el trabajo, hasta aqui funciona el mapa y los animales, pero a veces aparecen y otras no

--- villa/function.js
+++ villa/function.js
@@ -2,13 +2,27 @@ var v = document.getElementById("villa");
 var papel = v.getContext("2d");
 var mapa = "img/tile.png";
 
-var imagen = new Image();
-imagen.src =  mapa;
-imagen.addEventListener("load", dibujar);
+var fondo = new Image();
+fondo.src =  mapa;
+fondo.addEventListener("load", dibujar);
+
+
+var vaca = new Image();
+vaca.src = "img/vaca.png";
+vaca.addEventListener("load", dibujarVacas);
+
+var cerdo = new Image();
+cerdo.src = "img/cerdo.png";
+cerdo.addEventListener("load", dibujarCerdos);
+
+var pollo = new Image();
+pollo.src = "img/pollo.png";
+pollo.addEventListener("load", dibujarPollos);
+
 
 function dibujar()
 {
-	papel.drawImage(imagen, 0, 0);
+	papel.drawImage(fondo, 0, 0);
 }
 
 function aleatorio(min , max)
@@ -16,4 +30,20 @@ function aleatorio(min , max)
 	var resultado;
 	resultado =  Math.floor(Math.random() * (max - min + 1)) + min;
 	return resultado;
+}
+
+
+function dibujarVacas()
+{
+	papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarCerdos()
+{
+	papel.drawImage(cerdo, 10, 300);
+}
+
+function dibujarPollos()
+{
+	papel.drawImage(pollo, 300, 150);
 }
