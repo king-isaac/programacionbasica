b8c9a0cec157623a563b10617bcfe2836238f21e
King-isaac <isaacrequena@gmail.com>
2020-01-27 14:59:39 -0400

    tercer commit en el de trabajo, aqui funciona el mapa y las vacas

--- villa/function.js
+++ villa/function.js
@@ -1,28 +1,61 @@
 var v = document.getElementById("villa");
 var papel = v.getContext("2d");
-var mapa = "img/tile.png";
-
-var fondo = new Image();
-fondo.src =  mapa;
-fondo.addEventListener("load", dibujar);
 
 
-var vaca = new Image();
-vaca.src = "img/vaca.png";
-vaca.addEventListener("load", dibujarVacas);
+var fondo = {
+	url: "img/tile.png",
+	cargaOk: false
+};
 
-var cerdo = new Image();
-cerdo.src = "img/cerdo.png";
-cerdo.addEventListener("load", dibujarCerdos);
+var vaca = {
+	url: "img/vaca.png",
+	cargaOk: false
+};
 
-var pollo = new Image();
-pollo.src = "img/pollo.png";
-pollo.addEventListener("load", dibujarPollos);
+var cantidad = aleatorio (1, 5);
 
+fondo.imagen = new Image();
+fondo.imagen.src =  fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+
+vaca.objeto = new Image();
+vaca.objeto.src = vaca.url;
+vaca.objeto.addEventListener("load", cargarVacas);
+
+
+
+function cargarFondo()
+{
+	fondo.cargaOk = true;
+	dibujar();
+
+}
+
+function cargarVacas()
+{
+	vaca.cargaOk = true;
+	dibujar();
+
+}
 
 function dibujar()
 {
-	papel.drawImage(fondo, 0, 0);
+	if (fondo.cargaOk)
+	{
+		papel.drawImage(fondo.imagen, 0, 0);
+	}
+	if (vaca.cargaOk)
+	{
+		for(var c = 0; c < cantidad ; c++)
+		{
+		var x = aleatorio (0,7);
+		var y = aleatorio (0,7);
+		var x = x * 60;
+		var y = y * 60;
+		papel.drawImage(vaca.objeto, x, y);
+		}
+	}
 }
 
 function aleatorio(min , max)
@@ -30,20 +63,4 @@ function aleatorio(min , max)
 	var resultado;
 	resultado =  Math.floor(Math.random() * (max - min + 1)) + min;
 	return resultado;
-}
-
-
-function dibujarVacas()
-{
-	papel.drawImage(vaca, 10, 10);
-}
-
-function dibujarCerdos()
-{
-	papel.drawImage(cerdo, 10, 300);
-}
-
-function dibujarPollos()
-{
-	papel.drawImage(pollo, 300, 150);
 }
